898df49d850efacdadb96f2dcc7b31965ab62e4d
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2023-06-26 17:10:10 +0545

    updated codes

---
+++ test/uploadMassData.js
@@ -0,0 +1,27 @@
+const carCompanies = [
+    "Toyota",
+    "Volkswagen",
+    "General Motors",
+    "Ford",
+    "Honda",
+    "BMW",
+    "Mercedes-Benz",
+    "Nissan",
+    "Hyundai",
+    "Audi",
+    "Tesla",
+    "Fiat Chrysler Automobiles",
+    "Subaru",
+    "Volvo",
+    "Mazda",
+    "Kia",
+    "Jaguar Land Rover",
+    "Mitsubishi",
+    "Porsche",
+    "Peugeot",
+];
+
+carCompanies.forEach(async ele => {
+    // const uploads = await 
+    console.log(ele)
+})
